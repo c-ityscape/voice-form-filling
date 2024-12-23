2beea84709497435c918d39493d5ef3caa4441e9
c-ityscape <clarisisidore@gmail.com>
2024-12-24 00:34:30 +0400

    deployment to vercel

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def upload_audio():
         return jsonify({"message": f"Failed to process audio: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
